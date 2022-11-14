87ff8e7ee9ab2adc253a6fec362ccf389ee97b10
weizhou88 <117541284+weizhou88@users.noreply.github.com>
2022-11-14 01:45:28 +0000

    Deploying to public from @ weizhou88/kateplace@8beef97b422a2c753e2f26183f1378aebc084f1c 🚀

---
+++ _next/static/nG3HKv--GND1_gEQ4y4SK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6d48752644243048.css","static/chunks/pages/index-1cdf723554bbf321.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
