51bd94bea8fd63ec18cdd7c87a21692ee27c343e
weizhou88 <wzhou88@outlook.com>
2022-12-25 20:07:29 -0500

    develop index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import style from '../styles/index.module.css'
 
 export default function Home() {
   return (
@@ -9,25 +9,45 @@ export default function Home() {
         <title>Kateplace</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/x-icon" href="favicon.ico" />
-        <link rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
         <script src="https://kit.fontawesome.com/a076d05399.js"></script>
       </Head>
 
-      <div className= {styles.container_fluid}>
-        <nav className= {styles.nav}>
-          <div className= {styles.logo}>
-            <a href="./home"><img src="https://raw.githubusercontent.com/weizhou88/kateplace-asset/master/logo.png" /></a>
+      <div className= "container-fluid">
+        <nav className= "nav">
+          <div className= "logo">
+            <a href="./"><img src="https://raw.githubusercontent.com/weizhou88/kateplace-asset/master/logo.png" /></a>
           </div>
-          <div id="mainListDiv" className={styles.main_list}>
-            <ul className={styles.navlinks}>
-              <Link href={''} className={styles.link}>My drawings</Link>
-              <Link href={''} className={styles.link}>My paintings</Link>
-              <Link href={''} className={styles.link}>My graphics</Link>
-              <Link href={''} className={styles.link}>My animations</Link>
-            </ul>
+          <div id="mainListDiv" className="main_list">
+            <ul className="navlinks">
+              <li><Link href={''} className="link">My drawings</Link></li>
+              <li><Link href={''} className="link">My paintings</Link></li>
+              <li><Link href={''} className="link">My graphics</Link></li>
+              <li><Link href={''} className="link">My animations</Link></li>
+            </ul> 
           </div>
         </nav>
+        <div className= {style.jumbotron}>
+          <div className= {style.jumbotron_background}>
+            <img src="https://raw.githubusercontent.com/weizhou88/kateplace-asset/master/arts/drawings/a8/drawings53-512.png" className={style.blur} />
+            <img src="https://raw.githubusercontent.com/weizhou88/kateplace-asset/master/arts/drawings/a8/drawings60-512.png" className={`${style.blur} ${style.top}`} />
+          </div>
+
+          <div className={style.intro_container}>
+            <h3 className={style.intro_head} id={style.intro_head_big}>
+              You are in Kate place!
+            </h3>
+            <p className={style.intro_detail} id={style.intro_detail_big}>
+              Click the button to check out my art works!
+            </p>
+            <h3 className={style.intro_head} id={style.intro_head_small}>
+              You are in Kate place!
+            </h3>
+            <p className={style.intro_detail} id={style.intro_detail_small}>
+              Click the button to check out my art works.
+            </p>
+            <a href="./drawing" className="btn btn-primary">MY DRAWINGS</a>
+          </div>   
+        </div>
       </div>
     </div>
   )
